25b48982a7182556c3424b3c1c056fd24b6a0532
Mateus <mateusarruda232@gmail.com>
2021-01-03 19:42:35 -0300

    Changing port of localhost

--- src/main/java/com/mateus/ThemeParkApplication.java
+++ src/main/java/com/mateus/ThemeParkApplication.java
@@ -5,10 +5,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
+
 @SpringBootApplication
 public class ThemeParkApplication  {
     public static void main(String[] args) {
-        SpringApplication.run(ThemeParkApplication.class);
+        SpringApplication app = new SpringApplication(ThemeParkApplication.class);
+
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
+        app.run(args);
     }
     @Bean
     public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
