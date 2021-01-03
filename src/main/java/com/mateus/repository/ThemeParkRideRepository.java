9e8e8b38821dca3ed72127e4b82c9e887e681847
Mateus <mateusarruda232@gmail.com>
2021-01-03 17:32:30 -0300

    First Commit

---
+++ src/main/java/com/mateus/repository/ThemeParkRideRepository.java
@@ -0,0 +1,9 @@
+package com.mateus.repository;
+import com.mateus.entity.ThemeParkRide;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+@Repository
+public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
